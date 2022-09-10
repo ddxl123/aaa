3d8524b7aa1e0588759d2388f21a21118c876390
1033839760@qq.com <1033839760@qq.com>
2022-09-11 00:16:01 +0800

    AlgorithmAnalysis 类3

--- lib/test/DartTest.dart
+++ lib/test/DartTest.dart
@@ -1,8 +1,34 @@
+final logicalO = ['|', '&'];
+final relO = ['==', '!=', '<', '>', '<=', '>='];
+final re = RegExp(r'[\(\)]');
+
+/// 括号以及 index。
+final list = <Bracket2Index>[];
+
+class Bracket2Index {
+  final String bracket;
+  final int index;
+
+  Bracket2Index({required this.bracket, required this.index});
+}
+
 void main() {
-  String s = r'zaaif:xxx use: yyyzaa if:xxy use: yyy';
-  final vv = r'/if:/g';
-  for (var v in RegExp(r'if:[\S\s]*').allMatches(s)) {
-    print(s.substring(v.start, v.end));
-    print('object');
+  String s = r'a | (b != c) & ((z <= v | n == m) & ((l < j) | (l+1)/2 > 0))';
+  recursion(s);
+}
+
+void recursion(String s){
+  for (var v in re.allMatches(s)) {
+    final bra = s.substring(v.start, v.end);
+    if (bra == '(') {
+      list.add(Bracket2Index(bracket: bra, index: v.start));
+    } else if (bra == ')') {
+      final braInternal = s.substring(list.last.index + 1, v.start);
+      if (braInternal.contains(RegExp('[${logicalO.map((e) => e).join('')}]'))) {}
+    } else {
+      throw '未知异常！';
+    }
   }
 }
+
+
